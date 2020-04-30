52ab29fe7ccc7d53d8d92a64122df981d2a64a8e
Francisco Garcia <garciafran@hotmail.com>
2020-04-30 18:10:19 -0500

    Renderizando vistas

--- index.js
+++ index.js
@@ -1,38 +1,77 @@
 'use strict'
 
 const Hapi = require('hapi');
+const handlebars = require('handlebars');
 const config = require('./config');
+const inert = require('inert');
+const path = require('path');
+const vision = require('vision');
 
 const server = Hapi.server({
   port: config.port,
-  host: config.host
+  host: config.host.process,
+  routes: {
+    files: {
+      relativeTo: path.join(__dirname, 'public')
+    }
+  }
 });
 
 async function init () {
-  server.route({
-    method: 'GET',
-    path: '/',
-    handler: (req, h) => {
-      return h.response('Hola mundo mundial .....!!!').code(200);
-    }
-  });
 
-  server.route({
-    method: 'GET',
-    path: '/redirect',
-    handler: (req, h) => {
-      return h.redirect('http://platzi.com');
-    }
-  });
 
   try {
+    await server.register(inert);
+    await server.register(vision);
+
+    server.views({
+      engines: {
+        hbs: handlebars
+      },
+      relativeTo: __dirname, 
+      path: 'views',
+      layout: true,
+      layoutPath: 'views'
+    })
+
+    server.route({
+      method: 'GET',
+      path: '/',
+      handler: (req, h) => {
+        return h.view('index', {
+          title: 'home',
+        });
+      }
+    });
+
+    server.route({
+      method: 'GET',
+      path: '/register',
+      handler: (req, h) => {
+        return h.view('register', {
+          title: 'Registro',
+        });
+      }
+    });
+  
+    server.route({
+      method: 'GET',
+      path: '/{param*}',
+      handler: {
+        directory: {
+          path: '.',
+          index: ['index.html']
+        }
+      }
+    });
+
     await server.start();
   } catch (error) {
     console.error(error);
     process.exit(1);
   }
 
-  console.log(`Servidor lanzado en: ${server.info.uri}`);
+  console.log(`Servidor lanzado en: http://${config.host}:${config.port}`);
 }
 
 init();
